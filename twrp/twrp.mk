fa5851a7be0c85a14a3fa998689af639f13f73ae
Dan Pasanen <dan.pasanen@gmail.com>
2017-11-09 10:35:59 -0600

    mata: offset to the right by 64 pixels in twrp
    * until someone less lazy than me comes around and fixes this
      properly, shift it over so we can at least see everything (mostly)
    
    Change-Id: Ib5788f6c02b41fcf7957ca57785d521a82c9e58b

--- twrp/twrp.mk
+++ twrp/twrp.mk
@@ -13,6 +13,7 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_MAX_BRIGHTNESS := 255
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_hdpi
+TW_X_OFFSET := 64
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/soc/a800000.ssusb/a800000.dwc3/gadget/lun%d/file"
 #TW_BRIGHTNESS_PATH := "/sys/devices/soc/900000.qcom\x2cmdss_mdp/900000.qcom\x2cmdss_mdp:qcom\x2cmdss_fb_primary/leds/lcd-backlight/brightness"
