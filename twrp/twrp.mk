ec29cd9c4982b15aaaf416f2d72237761b97acf7
Desterly <d3s7royer@gmail.com>
2017-11-09 10:04:46 -0600

    mata: fixes for twrp
    * Fix touch
    * Custom theme to handle camera cutout
    * Initial crypto work (still not working)
    
    Change-Id: I7b653c6e6b1db9b0bb626d7bc57202f6f9ad6a17

--- twrp/twrp.mk
+++ twrp/twrp.mk
@@ -1,9 +1,10 @@
 RECOVERY_SDCARD_ON_DATA := true
+TARGET_HW_DISK_ENCRYPTION := true
 TARGET_RECOVERY_FSTAB := device/essential/mata/twrp/twrp.fstab
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USES_LOGD := true
-TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
+TW_CUSTOM_THEME := device/essential/mata/twrp/theme
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXCLUDE_SUPERSU := true
 TW_DEFAULT_BRIGHTNESS := 149
@@ -15,5 +16,4 @@ TW_THEME := portrait_hdpi
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/soc/a800000.ssusb/a800000.dwc3/gadget/lun%d/file"
 #TW_BRIGHTNESS_PATH := "/sys/devices/soc/900000.qcom\x2cmdss_mdp/900000.qcom\x2cmdss_mdp:qcom\x2cmdss_fb_primary/leds/lcd-backlight/brightness"
-#TARGET_HW_DISK_ENCRYPTION := true
 #TARGET_KEYMASTER_WAIT_FOR_QSEE := true
