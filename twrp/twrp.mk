066819ea4593185ee9e10592338bdeb2dd515fc0
Dan Pasanen <dan.pasanen@gmail.com>
2017-12-01 09:20:17 -0600

    mata: squash twrp support for crypto and touchscreen
    * Unfortunately we still have to ship some blobs in the device tree
      because blobs that we ship from vendor rely on them and they're only
      built for system.
    * Sure I could go through the hell of hacking makefiles all over the
      platform in order to try and build these on the fly for recovery, but
      this seems way cleaner.
    
    Squash of the following commits:
    
    commit a10297b54e73fa576a144831f463eb2a39abd1fd
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Fri Dec 1 09:17:36 2017 -0600
    
        twrp: copy blobs that we can from vendor
    
        Change-Id: Ie818b157fa424bf8645ef667b94de73bbd9eb245
    
    commit 9e43833b0242a657dc7bc5390f7fc3e147d56190
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Fri Dec 1 07:44:46 2017 -0600
    
        mata: remove TARGET_HW_DISK_ENCRYPTION for twrp
    
        * This is unecessary and causes build breakage
    
        Change-Id: I7efac48b3e060d38e25380f7a46bcb216da99df0
    
    commit 9d67d5d1fdd99dc666f180069c6d392281e3c76b
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Thu Nov 9 20:40:11 2017 -0600
    
        mata: use a simple PRODUCT_COPY_FILES for ui.xml
    
        * As far as I can tell there's no one simple way to override
          just one xml from a theme. Let's not get crazy and copy an
          entire theme here and just go with this.
    
        Change-Id: Ib5631f744a3e8d53760f016dfecd9665ded1edbd
    
    commit dad5740f4347ae8f3e5935fe6809c6559c825ef2
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Thu Nov 9 20:09:42 2017 -0600
    
        mata: remove mkdirs from recovery init
    
        * These are bad news for crypto. Shit fails, we don't need them,
          move on with your life.
    
        Change-Id: If99d4237fc63b2fe0a55f736e4c81e9da15d2932
    
    commit 06b2622c91e47cf09a4466339d28de1f99994c03
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Thu Nov 9 19:54:24 2017 -0600
    
        mata: clean up twrp flags
    
        Change-Id: I9d45d57eda5d59f39c1bbcad8e5fdfc7ae6d7033
    
    commit 55c961e7749f75d0ed7a21713523de252e835eef
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Thu Nov 9 19:39:26 2017 -0600
    
        mata: enable install_keyring
    
        Change-Id: I7a1d898d5e4b9fcdab43d6d05cc91a15192c72a9
    
    commit f6166ffc8312ea5ff2e5c66d57da832f9dc6e528
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Thu Nov 9 19:03:32 2017 -0600
    
        mata: kang qseecomd from cheeseburger, who kanged from sagit
    
        Change-Id: I7050598d654e607467c59e425e9e38b38f6845dd
    
    commit 75b4b72d6393e262caf80286afdc16cbfcc92502
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Thu Nov 9 19:46:35 2017 -0600
    
        mata: use TWRP_THEME_LOC, we dont need to replace the ENTIRE theme
    
        Change-Id: Id84963a77664fa7822350e4dbf2c2b314b386ec0
    
    commit 9654acd12fc7cf83a5bd21e7bdb1fb0aee5f518f
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Thu Nov 9 12:01:09 2017 -0600
    
        mata: fix bootctrl hal in twrp
    
        Change-Id: I9725c862ba2ab115d15eceb82a8b7d30c9c21f23
    
    commit fa5851a7be0c85a14a3fa998689af639f13f73ae
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Thu Nov 9 10:35:59 2017 -0600
    
        mata: offset to the right by 64 pixels in twrp
    
        * until someone less lazy than me comes around and fixes this
          properly, shift it over so we can at least see everything (mostly)
    
        Change-Id: Ib5788f6c02b41fcf7957ca57785d521a82c9e58b
    
    commit 097fb169feae5d826472a16ca586b6b191b8a9d2
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Thu Nov 9 10:18:14 2017 -0600
    
        mata: enable usb otg in twrp
    
        Change-Id: I6ac480341244bd63788d4778c8872acce0413a1d
    
    commit ec29cd9c4982b15aaaf416f2d72237761b97acf7
    Author: Desterly <d3s7royer@gmail.com>
    Date:   Thu Nov 9 10:04:46 2017 -0600
    
        mata: fixes for twrp
    
        * Fix touch
        * Custom theme to handle camera cutout
        * Initial crypto work (still not working)
    
        Change-Id: I7b653c6e6b1db9b0bb626d7bc57202f6f9ad6a17
    
    Change-Id: Ic14216aa9331c5bf3c7aca42ca23341c2db4e418

--- twrp/twrp.mk
+++ twrp/twrp.mk
@@ -3,7 +3,6 @@ TARGET_RECOVERY_FSTAB := device/essential/mata/twrp/twrp.fstab
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USES_LOGD := true
-TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXCLUDE_SUPERSU := true
 TW_DEFAULT_BRIGHTNESS := 149
@@ -11,9 +10,71 @@ TW_INCLUDE_CRYPTO := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_MAX_BRIGHTNESS := 255
 TW_SCREEN_BLANK_ON_BOOT := true
-TW_THEME := portrait_hdpi
+TW_X_OFFSET := 64
 TWRP_INCLUDE_LOGCAT := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/soc/a800000.ssusb/a800000.dwc3/gadget/lun%d/file"
-#TW_BRIGHTNESS_PATH := "/sys/devices/soc/900000.qcom\x2cmdss_mdp/900000.qcom\x2cmdss_mdp:qcom\x2cmdss_fb_primary/leds/lcd-backlight/brightness"
-#TARGET_HW_DISK_ENCRYPTION := true
-#TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+TWRP_THEME_LOC := device/essential/mata/twrp/theme
+
+PRODUCT_COPY_FILES += \
+    vendor/essential/mata/proprietary/etc/firmware/hbtp/hbtpcfg_qtc800s.dat:recovery/root/etc/firmware/hbtp/hbtpcfg_qtc800s.dat \
+    vendor/essential/mata/proprietary/etc/firmware/hbtp/hbtpcfg_rohm.dat:recovery/root/etc/firmware/hbtp/hbtpcfg_rohm.dat \
+    vendor/essential/mata/proprietary/etc/firmware/hbtp/loader.cfg:recovery/root/etc/firmware/hbtp/loader.cfg \
+    vendor/essential/mata/proprietary/etc/firmware/hbtp/qtc800s_dsp.bin:recovery/root/etc/firmware/hbtp/qtc800s_dsp.bin \
+    vendor/essential/mata/proprietary/bin/adsprpcd:recovery/root/sbin/adsprpcd \
+    vendor/essential/mata/proprietary/bin/qseecomd:recovery/root/sbin/qseecomd \
+    vendor/essential/mata/proprietary/vendor/bin/hbtp_daemon:recovery/root/sbin/hbtp_daemon \
+    vendor/essential/mata/proprietary/vendor/lib64/hw/gatekeeper.msm8998.so:recovery/root/vendor/lib64/hw/gatekeeper.msm8998.so \
+    vendor/essential/mata/proprietary/vendor/lib64/hw/keystore.msm8998.so:recovery/root/vendor/lib64/hw/keystore.msm8998.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libadsp_default_listener.so:recovery/root/sbin/libadsp_default_listener.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libadsprpc.so:recovery/root/sbin/libadsprpc.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libdiag.so:recovery/root/vendor/lib64/libdiag.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libdrmfs.so:recovery/root/vendor/lib64/libdrmfs.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libdrmtime.so:recovery/root/vendor/lib64/libdrmtime.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libfastrpc_utf_stub.so:recovery/root/sbin/libfastrpc_utf_stub.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libGPreqcancel.so:recovery/root/vendor/lib64/libGPreqcancel.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libGPreqcancel_svc.so:recovery/root/vendor/lib64/libGPreqcancel_svc.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libqisl.so:recovery/root/vendor/lib64/libqisl.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libhbtpclient.so:recovery/root/sbin/libhbtpclient.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libhbtpdsp.so:recovery/root/sbin/libhbtpdsp.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libhbtpfrmwk.so:recovery/root/sbin/libhbtpfrmwk.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libQSEEComAPI.so:recovery/root/vendor/lib64/libQSEEComAPI.so \
+    vendor/essential/mata/proprietary/vendor/lib64/librpmb.so:recovery/root/vendor/lib64/librpmb.so \
+    vendor/essential/mata/proprietary/vendor/lib64/lib-sec-disp.so:recovery/root/vendor/lib64/lib-sec-disp.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libSecureUILib.so:recovery/root/sbin/libSecureUILib.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libSecureUILib.so:recovery/root/vendor/lib64/libSecureUILib.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libsecureui.so:recovery/root/sbin/libsecureui.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libsecureui.so:recovery/root/vendor/lib64/libsecureui.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libsecureui_svcsock.so:recovery/root/vendor/lib64/libsecureui_svcsock.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libspl.so:recovery/root/vendor/lib64/libspl.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libssd.so:recovery/root/vendor/lib64/libssd.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libStDrvInt.so:recovery/root/vendor/lib64/libStDrvInt.so \
+    vendor/essential/mata/proprietary/vendor/lib64/libtime_genoff.so:recovery/root/vendor/lib64/libtime_genoff.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libadsp_fd_skel.so:recovery/root/vendor/lib/rfsa/adsp/libadsp_fd_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libadsp_hvx_add_constant.so:recovery/root/vendor/lib/rfsa/adsp/libadsp_hvx_add_constant.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libadsp_hvx_skel.so:recovery/root/vendor/lib/rfsa/adsp/libadsp_hvx_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libapps_mem_heap.so:recovery/root/vendor/lib/rfsa/adsp/libapps_mem_heap.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libdspCV_skel.so:recovery/root/vendor/lib/rfsa/adsp/libdspCV_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libedge_smooth_skel.so:recovery/root/vendor/lib/rfsa/adsp/libedge_smooth_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libfastcvadsp_skel.so:recovery/root/vendor/lib/rfsa/adsp/libfastcvadsp_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libfastcvadsp.so:recovery/root/vendor/lib/rfsa/adsp/libfastcvadsp.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libFastRPC_UTF_Forward_skel.so:recovery/root/sbin/libFastRPC_UTF_Forward_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libFastRPC_UTF_Forward_skel.so:recovery/root/vendor/lib/rfsa/adsp/libFastRPC_UTF_Forward_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libobjectMattingApp_skel.so:recovery/root/vendor/lib/rfsa/adsp/libobjectMattingApp_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libqvr_adsp_driver_skel.so:recovery/root/vendor/lib/rfsa/adsp/libqvr_adsp_driver_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libscveBlobDescriptor_skel.so:recovery/root/vendor/lib/rfsa/adsp/libscveBlobDescriptor_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libscveCleverCapture_skel.so:recovery/root/vendor/lib/rfsa/adsp/libscveCleverCapture_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libscveFaceLandmark_skel.so:recovery/root/vendor/lib/rfsa/adsp/libscveFaceLandmark_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libscveFaceRecognition_skel.so:recovery/root/vendor/lib/rfsa/adsp/libscveFaceRecognition_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libscveObjectSegmentation_skel.so:recovery/root/vendor/lib/rfsa/adsp/libscveObjectSegmentation_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libscveT2T_skel.so:recovery/root/vendor/lib/rfsa/adsp/libscveT2T_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libscveTextReco_skel.so:recovery/root/vendor/lib/rfsa/adsp/libscveTextReco_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libscveVideoSummary_skel.so:recovery/root/vendor/lib/rfsa/adsp/libscveVideoSummary_skel.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libvpp_frc.so:recovery/root/vendor/lib/rfsa/adsp/libvpp_frc.so \
+    vendor/essential/mata/proprietary/vendor/lib/rfsa/adsp/libvpp_svc_skel.so:recovery/root/vendor/lib/rfsa/adsp/libvpp_svc_skel.so
+
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += \
+    $(TARGET_RECOVERY_ROOT_OUT)/sbin/adsprpcd \
+    $(TARGET_RECOVERY_ROOT_OUT)/sbin/hbtp_daemon \
+    $(TARGET_RECOVERY_ROOT_OUT)/sbin/qseecomd
+
+PRODUCT_COPY_FILES += \
+    device/essential/mata/twrp/theme/ui.xml:recovery/root/twres/ui.xml
