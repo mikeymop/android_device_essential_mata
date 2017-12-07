63ca21707b5db82d84bb4355d93c6ccbde059ec7
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 15:15:00 +0100

    mata: Build a dummy android.hidl.base@1.0 for Oreo-MR0 blobs
    Change-Id: I50d02364368691da2bc081988d500587cc427852

--- device.mk
+++ device.mk
@@ -98,6 +98,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0-java
+
 # Init
 PRODUCT_PACKAGES += \
     init.class_main.sh \
